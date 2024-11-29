082659dd9e8ae26ae06754485d5877565c7cf61c
akesh-1997 <a.triple93@yahoo.com>
2024-11-29 15:01:05 +0530

    check currency

--- auth.php
+++ auth.php
@@ -10,20 +10,20 @@ $name = isset($_POST['name']) ? $_POST['name'] : 'Customer';
 $reference = isset($_POST['reference']) ? $_POST['reference'] : 'No reference';
 $email = isset($_POST['email']) ? $_POST['email'] : ''; // Fetch the actual email value
 $price = isset($_POST['price']) ? floatval($_POST['price']) : 0.0;
-$currency = isset($_POST['currency']) ? strtoupper($_POST['currency']) : 'LKR';
-$amount = $price > 0 ? intval($price * 100) : 0;
+$currency_p = isset($_POST['currency']) ? strtoupper($_POST['currency']) : 'LKR';
+$amount_p = $price > 0 ? intval($price * 100) : 0;
 
 if (isset($_POST['simplifyToken'])) {
     $token = $_POST['simplifyToken'];
 
-    if ($currency == 'LKR') {
+
+    if ($currency_p == 'LKR') {
         Simplify::$publicKey = SMPLY_LKR_PUBKEY;
         Simplify::$privateKey = SMPLY_LKR_PVKEY;
     } else {
         Simplify::$publicKey = SMPLY_USD_PUBKEY;
         Simplify::$privateKey = SMPLY_USD_PVKEY;
     }
-
     if (empty($email)) {
         $notificationMessage = 'No email address provided.';
         $status = 'ERROR';
@@ -31,12 +31,11 @@ if (isset($_POST['simplifyToken'])) {
         try {
             $payment = Simplify_Payment::createPayment(array(
                 'reference' => $reference,
-                'amount' => $amount,
+                'amount' => $amount_p,
                 'description' => 'Payment description',
-                'currency' => $currency,
+                'currency' => $currency_p,
                 'token' => $token,
             ));
-
             $mail = new PHPMailer(true);
             try {
                 $mail->SMTPDebug = 2;
@@ -54,19 +53,23 @@ if (isset($_POST['simplifyToken'])) {
                 foreach (CC_LIST as $cc) {
                     $mail->AddCC($cc);
                 }
-
                 $mail->isHTML(false);
 
                 if ($payment->paymentStatus == 'APPROVED') {
                     $transactionId = $payment->id;
                     $declineReason = $payment->declineReason;
+                    $currency = $payment->currency;
+                    $amount = $payment->amount;
                     $notificationMessage = "Your payment for " . htmlspecialchars($reference) . " has been Approved. Thank you!";
                     $status = 'APPROVED';
+
+                    $getprice = $amount / 100;
+                    error_log("currency:$currency");
 
                     // Approved payment email
                     $mail->Subject = "PaySafe - Payment Confirmation for Ref- " . htmlspecialchars($reference) . "";
                     $mail->Body = "Dear " . htmlspecialchars($name) . ",\n\nYour payment of " .
-                        ($currency == 'USD' ? '$' : 'LKR ') . number_format($price, 2) .
+                        ($currency == 'USD' ? '$' : 'LKR ') . number_format($getprice, 2) .
                         " has been successfully approved." .
                         "Transaction ID: " . htmlspecialchars($transactionId) . "\n\n" .
                         "Thank you for your purchase!";
@@ -74,7 +77,9 @@ if (isset($_POST['simplifyToken'])) {
                     $notificationMessage = 'Payment Failed: ' . htmlspecialchars($payment->paymentStatus);
                     $status = 'FAILED';
                     $transactionId = $payment->id;
-
+                    $currency = $payment->currency;
+                    $amount = $payment->amout;
+                    $getprice = $amount / 100;
 
                     // Declined payment email
                     $mail->Subject = "PaySafe - Payment failed  Ref-" . htmlspecialchars($reference) . "\n\n";
@@ -100,7 +105,6 @@ if (isset($_POST['simplifyToken'])) {
 }
 
 // Redirect back to payment page with status and message
-header("Location: " . BASE_PATH . "?status=$status&message=" . urlencode($notificationMessage));
-
+header("Location:" . BASE_PATH . "status=$status&message=" . urlencode($notificationMessage));
 
 exit();
