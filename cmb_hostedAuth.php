563913446cfa6b7066d9ab0fdafc7fb457791483
akesh-1997 <a.triple93@yahoo.com>
2025-02-19 16:30:08 +0530

    make authpage

--- cmb_hostedAuth.php
+++ cmb_hostedAuth.php
@@ -1,50 +1,72 @@
-
 <?php
+require_once 'config/config.php';
 
-$orderid = "10601";
-$apiPassword = "CBCTEST";
-$merchant = "TESTMALKEYRENLKR";
-$amount = "11.00";
-//$returnUrl = "http://cmbgateway.loc/";
-$currency = "LKR";
+$amount = isset($_GET['amount']) ? $_GET['amount'] : "1.00";
+$currency = isset($_GET['currency']) ? $_GET['currency'] : "USD";
+$description = isset($_GET['description']) ? $_GET['description'] : "no description";
+$orderId =  isset($_GET['orderId']) ? $_GET['orderId'] : "no order id";
+
+$url = "https://nationstrustbankplc.gateway.mastercard.com/api/rest/version/81/merchant/" . MERCHANT_ID . "/session";
+
+$data = [
+    "apiOperation" => "INITIATE_CHECKOUT",
+    "checkoutMode" => "WEBSITE",
+    "interaction" => [
+        "operation" => "AUTHORIZE",
+        "merchant" => [
+            "name" => "merchant.TEST9170372718",
+            "logo" =>  "https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg",
+           // "logo" =>  "https://findit-resources.s3.us-east-2.amazonaws.com/account/profilePictures/1628852770756.png", 
+            "url" => "https://www.malkey.lk",
+            "phone" => "+94-112365365",
+            "email" => "info@malkey.lk"
+          ],
+        //"returnUrl" => "https://www.malkey.lk",
+
+        "locale" => "en_US",
+        "style" => [
+            "theme" => "default",
+  ]
+ ],
+    "order" => [
+        "currency" => $currency,
+        "amount" => $amount,
+        "id" => $orderId,
+        "description" => $description
+    ]
+];
+
+$options = [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: text/plain",
+        "Authorization: Basic " . base64_encode(API_USERNAME . ":" . API_PASSWORD)
+    ],
+    
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_FAILONERROR => true
+];
 
 $ch = curl_init();
+curl_setopt_array($ch, $options);
+$response = curl_exec($ch);
 
-curl_setopt($ch, CURLOPT_URL, 'https://cbcmpgs.gateway.mastercard.com/api/nvp/version/57');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
+if ($response === false) {
+    $error_msg = curl_error($ch);
+    $error_msg = curl_strerror(curl_errno($ch));
+    error_log($error_msg);
 
-curl_setopt(
-    $ch,
-    CURLOPT_POSTFIELDS,
-    "apiOperation=CREATE_CHECKOUT_SESSION&" .
-        "apiPassword=$apiPassword&" .
-        "interaction.returnUrl=$returnUrl&" .
-        "interaction.operation=PURCHASE&" .
-        "apiUsername=merchant.$merchant&" .
-        "merchant=$merchant&" .
-        "order.id=$orderid&" .
-        "order.amount=$amount&" .
-        "order.currency=$currency"
-);
-
-$headers = array();
-$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'ERROR: ' . curl_error($ch);
-} else {
-    echo $result;
+    curl_close($ch);
+    die("cURL error: " . $error_msg);
 }
 curl_close($ch);
 
-curl_close($ch);
-print_r($ch);
-
-
-$sessionid = explode("=", explode("&", $result)[2])[1];
-
-
-?>
+$result = json_decode($response, true);
+if (isset($result['session']['id'])) {
+    $sessionId = $result['session']['id'];
+} else {
+    die("Failed to create session: " . json_encode($result));
+}
