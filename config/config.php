f81ac46ed0138ac368a5f8ec74d6613142170cc8
akesh-1997 <a.triple93@yahoo.com>
2025-02-19 15:58:07 +0530

    final ntb payment gateway

--- config/config.php
+++ config/config.php
@@ -2,34 +2,13 @@
 
 define('APP_LIVE', false);
 define('BASE_PATH', '/paysafe');
-define('CC_LIST', ['viraj.abayarathna@gmail.com', 'milindum@gmail.com', 'accounts@malkey.lk' ,'piumal0713@gmail.com']);
 
-
-
-define('MERCHANT_ID', 'TEST9170372718');
-define('API_PASSWORD', '6643edaf7600027f17cddbd661db156b');
-
-
-ini_set('display_errors', 0);
-if (session_status() == PHP_SESSION_NONE) {
-    session_set_cookie_params([
-        'lifetime' => 3600,
-        'path' => '/',
-        'domain' => '.cmbgateway.loc',
-        'secure' => true,
-        'httponly' => true,
-        'samesite' => 'Strict'
-    ]);
-    session_start();
+if (APP_LIVE) {
+    define('MERCHANT_ID', '9170372718'); // live 
+    define('API_USERNAME', 'merchant.TEST9170372718'); 
+    define('API_PASSWORD', '2bc2cac63cef6ebf59c7c925e571ee49'); 
+} else {
+    define('MERCHANT_ID', 'TEST9170372718'); // sandbox 
+    define('API_USERNAME', 'merchant.TEST9170372718'); 
+    define('API_PASSWORD', '9561cde89b146e22afd2dbec7d145a4f'); 
 }
-
-
-
-define('MAIL_DRIVER', 'smtp');
-define('MAIL_HOST', 'email-smtp.us-east-1.amazonaws.com');
-define('MAIL_PORT', 465);
-define('MAIL_ENCRYPTION', 'ssl');
-define('MAIL_USERNAME', 'AKIA5K7Q37VYYJEFNMN2');
-define('MAIL_PASSWORD', 'BHwtncYWVjdoVtd5Y9Epu1/UBPV7fRi+zbblftJlqabg');
-define('MAIL_ADDRESS', 'rype3-dtaas-platform@rype3.com');
-define('MAIL_NAME', 'DT Plutos');
--- ntb_sessionAuth.php
+++ ntb_sessionAuth.php
@@ -1,14 +1,12 @@
 <?php
+require_once 'config/config.php';
 
 $amount = isset($_GET['amount']) ? $_GET['amount'] : "1.00";
 $currency = isset($_GET['currency']) ? $_GET['currency'] : "USD";
 $description = isset($_GET['description']) ? $_GET['description'] : "no description";
 $orderId =  isset($_GET['orderId']) ? $_GET['orderId'] : "no order id";
-$merchantId = "TEST9170372718";
-$apiUsername = "merchant.TEST9170372718";
-$apiPassword = "9561cde89b146e22afd2dbec7d145a4f";
 
-$url = "https://nationstrustbankplc.gateway.mastercard.com/api/rest/version/81/merchant/$merchantId/session";
+$url = "https://nationstrustbankplc.gateway.mastercard.com/api/rest/version/81/merchant/" . MERCHANT_ID . "/session";
 
 $data = [
     "apiOperation" => "INITIATE_CHECKOUT",
@@ -18,22 +16,18 @@ $data = [
         "merchant" => [
             "name" => "merchant.TEST9170372718",
             "logo" =>  "https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg",
-           // "logo" =>  "https://findit-resources.s3.us-east-2.amazonaws.com/account/profilePictures/1628852770756.png",
-
-          
+           // "logo" =>  "https://findit-resources.s3.us-east-2.amazonaws.com/account/profilePictures/1628852770756.png", 
             "url" => "https://www.malkey.lk",
             "phone" => "+94-112365365",
             "email" => "info@malkey.lk"
-        ],
+          ],
         //"returnUrl" => "https://www.malkey.lk",
 
         "locale" => "en_US",
         "style" => [
             "theme" => "default",
-
-
-        ]
-    ],
+  ]
+ ],
     "order" => [
         "currency" => $currency,
         "amount" => $amount,
@@ -49,8 +43,9 @@ $options = [
     CURLOPT_POSTFIELDS => json_encode($data),
     CURLOPT_HTTPHEADER => [
         "Content-Type: text/plain",
-        "Authorization: Basic " . base64_encode("$apiUsername:$apiPassword")
+        "Authorization: Basic " . base64_encode(API_USERNAME . ":" . API_PASSWORD)
     ],
+    
     CURLOPT_SSL_VERIFYPEER => false,
     CURLOPT_FAILONERROR => true
 ];
@@ -67,14 +62,11 @@ if ($response === false) {
     curl_close($ch);
     die("cURL error: " . $error_msg);
 }
-
 curl_close($ch);
 
 $result = json_decode($response, true);
 if (isset($result['session']['id'])) {
     $sessionId = $result['session']['id'];
-
-    // echo json_encode(["sessionId" => $sessionId]);
 } else {
     die("Failed to create session: " . json_encode($result));
 }
