24c248b3f3a600ed79d97f9dbd20b21f7a0695ea
akesh-1997 <a.triple93@yahoo.com>
2025-03-14 09:57:41 +0530

    change image basepath

--- config/config.php
+++ config/config.php
@@ -4,28 +4,39 @@ define('APP_LIVE', false);
 define('BASE_PATH', '/paysafe');
 
 
-define('LOGO', 'https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg'); 
+define('LOGO', 'https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg');
 
 
-if (!defined('APP_LIVE')) define('APP_LIVE', false); 
+if (!defined('APP_LIVE')) define('APP_LIVE', false);
 if (APP_LIVE) {
     define('MERCHANT_ID_USD', ''); // live 
-    define('API_USERNAME_USD', ''); 
-    define('API_PASSWORD_USD', ''); 
+    define('API_USERNAME_USD', '');
+    define('API_PASSWORD_USD', '');
 
     define('MERCHANT_ID_LKR', ''); //live 
-    define('API_USERNAME_LKR', ''); 
-    define('API_PASSWORD_LKR', ''); 
+    define('API_USERNAME_LKR', '');
+    define('API_PASSWORD_LKR', '');
 } else {
     define('MERCHANT_ID_LKR', 'TESTMALKEYRENLKR'); // sandbox 
-    define('API_USERNAME_LKR', 'merchant.TESTMALKEYRENLKR'); 
-    define('API_PASSWORD_LKR', '0778afc55fa88712010a6e258f60c565'); 
+    define('API_USERNAME_LKR', 'merchant.TESTMALKEYRENLKR');
+    define('API_PASSWORD_LKR', '0778afc55fa88712010a6e258f60c565');
 
     define('MERCHANT_ID_USD', 'TESTMALKEYRENUSD'); // sandbox 
-    define('API_USERNAME_USD', 'merchant.TESTMALKEYRENUSD'); 
-    define('API_PASSWORD_USD', 'a0524267d0593d281975c7e69bed8bd4'); 
+    define('API_USERNAME_USD', 'merchant.TESTMALKEYRENUSD');
+    define('API_PASSWORD_USD', 'a0524267d0593d281975c7e69bed8bd4');
 }
-define('NAME', 'Malkey Rent A Car'); 
+
+
+if (APP_LIVE) {
+
+    define('ASSET_PATH_URL', 'https://malkey.go.digitable.io/paysafe/ntb/');
+} else {
+    define('ASSET_PATH_URL', 'http://http://cmbgateway.loc/paysafe');
+}
+
+
+
+define('NAME', 'Malkey Rent A Car');
 
 define('CC_LIST', ['thamara.dasun1@gmail.com']);
 
