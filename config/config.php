4da63a53890351ada60ad1464f41b5ae52ad0b5f
akesh-1997 <a.triple93@yahoo.com>
2025-03-20 16:39:47 +0530

    make live true

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_LIVE',false);   
+define('APP_LIVE',true);   
 define('BASE_PATH', '/paysafe/ntb');
 
 define('LOGO', 'https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg'); 
