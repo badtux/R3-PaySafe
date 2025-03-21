7d95b01253b9303386a7f2a66b352bbe33b3a3d6
akesh-1997 <a.triple93@yahoo.com>
2025-03-21 09:11:54 +0530

    make config sample file

--- config/config.php
+++ config/config.php
@@ -21,8 +21,7 @@ if (APP_LIVE) {
 }
 define('NAME', 'Malkey Rent A Car');
 
-define('CC_LIST', ['thamara.dasun1@gmail.com']);
-//define('CC_LIST', ['viraj.abayarathna@gmail.com', 'milindum@gmail.com', 'accounts@malkey.lk' ,'piumal0713@gmail.com']);
+define('CC_LIST', ['viraj.abayarathna@gmail.com', 'milindum@gmail.com', 'accounts@malkey.lk' ,'piumal0713@gmail.com']);
 define('MAIL_DRIVER', 'smtp');
 define('MAIL_HOST', 'email-smtp.us-east-1.amazonaws.com');
 define('MAIL_PORT', 465);
--- ntb_sessionAuth.php
+++ ntb_sessionAuth.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'config/config.php';
+//require_once 'config/config.sample.php';
+
 require 'vendor/autoload.php';
 
 class ntbToken {
