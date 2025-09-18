b273f4b9d07cfd579e5f45b1a44f88596836511a
akesh-1997 <a.triple93@yahoo.com>
2025-09-18 15:51:58 +0530

    change ip

--- config/config.php
+++ config/config.php
@@ -16,7 +16,7 @@ if (APP_LIVE) {
     define('API_PASSWORD_LKR', '8ac724a6d1a9b99f4060c808142d47c6');
 
 
-    define('DATABASE_URL', 'mongodb://172.0.0.1:27017');
+    define('DATABASE_URL', 'mongodb://127.0.0.1:27017');
     define('COLLECTION', 'paysafe');
     define('DB', 'malky');
 
