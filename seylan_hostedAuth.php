35cb2b64c65be09d080dc6daf79b8ca30dc60bde
yushapiumal <adypiumalhpbk0701@gmail.com>
2025-10-09 17:21:02 +0530

    add bank name to db

--- seylan_hostedAuth.php
+++ seylan_hostedAuth.php
@@ -132,7 +132,7 @@ if (!$txnId) {
                         'description' => $description,
                         'merchantId' => $merchantId,
                         'sessionId' => $sessionId,
-                           'bank'   => "seylan",
+                        'bank'   => "seylan",
                         'createdAt' => new UTCDateTime()
                     ]);
 
