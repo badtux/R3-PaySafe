8451268f3e2fbbd8359724d6c650423ad1bb2a1c
badtux <viraj@ceynet.asia>
2026-01-02 04:50:46 +0530

    added more logic

--- seylan_hosted.php
+++ seylan_hosted.php
@@ -35,7 +35,7 @@ function initiateCheckout($txnId, $logger) {
     $endPointUrl = IPG_API_URL.'/'.MERCHANT_ID.'/session';
 
     $data = [
-        "apiOperation" => "INITIATE_CHECKOUT",
+        "apiOperation" => "CREATE_CHECKOUT_SESSION",
         "checkoutMode" => "WEBSITE",
         "interaction" => [
             "operation" => "AUTHORIZE",
