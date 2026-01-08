8dfe67efcc5a791eb6cde655ec89f01c24acc636
yushapiumal <adypiumalhpbk0701@gmail.com>
2026-01-08 10:25:54 +0530

    add bcc list

--- config/config.sample.php
+++ config/config.sample.php
@@ -1,24 +1,22 @@
 <?php
 
-define('APP_LIVE',false);
-define('BASE_PATH','/cmb');
-
-define('LOGO', 'https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg');
+define('APP_LIVE',true);
+define('BASE_PATH','/paysafe/cmb');
 
 $host = $_SERVER['HTTP_HOST']; 
-$tenant = explode('.', $host)[0]; 
+$tenant = explode('.', $host)[0];
 
 if ($tenant === 'localhost' || empty($tenant)) {
     $tenant = 'default';
-
 }
 
 if (!defined('TENANT')) {
     define('TENANT', $tenant);
 }
+
 $databaseName = "{$tenant}_paysafe";
 
-
+define('LOGO', 'https://static.wixstatic.com/media/c7b147_b3d1abb02b5346b68d176a13f1ae27d5~mv2.jpg/v1/fill/w_847,h_807,al_c,q_85/Malkey%20Logo%20Red%20-%20Milindu%20Mallawaratchie.jpg');
 
 if (APP_LIVE) {
     define('MERCHANT_ID_USD', 'MALKEYRENUSD'); // live 
@@ -30,10 +28,10 @@ if (APP_LIVE) {
     define('API_USERNAME_LKR', 'merchant.MALKEYRENLKR');
     define('API_PASSWORD_LKR', '8ac724a6d1a9b99f4060c808142d47c6');
 
-     define('DATABASE_URL', 'mongodb+srv://piumal0713:Adyp%400713@cluster0.8bv15.mongodb.net/?retryWrites=true&w=majority&authSource=admin');
-     define('COLLECTION', 'payments');
-     define('DB', $databaseName);
-    
+
+    define('DATABASE_URL', 'mongodb://192.168.167.75:27017');
+    define('COLLECTION', 'payments');
+    define('DB', $databaseName);
 
 } else {
     define('MERCHANT_ID_LKR', 'TESTMALKEYRENLKR'); // sandbox 
@@ -44,13 +42,13 @@ if (APP_LIVE) {
     define('API_USERNAME_USD', 'merchant.TESTMALKEYRENUSD');
     define('API_PASSWORD_USD', 'a0524267d0593d281975c7e69bed8bd4');
     define('REDIRECT_URL', "http://{$tenant}.paymentgateway.loc/cmb/status");
-    
-    define('DATABASE_URL', 'mongodb+srv://piumal0713:Adyp%400713@cluster0.8bv15.mongodb.net/?retryWrites=true&w=majority&authSource=admin');
-    define('COLLECTION', 'payments');
-    define('DB', $databaseName);
+
+
+     define('DATABASE_URL', 'mongodb+srv://piumal0713:Adyp%400713@cluster0.8bv15.mongodb.net/?retryWrites=true&w=majority&authSource=admin');
+     define('COLLECTION', 'payments');
+     define('DB', $databaseName);
     
 }
-
 
 
 if (APP_LIVE) {
@@ -60,10 +58,9 @@ if (APP_LIVE) {
     define('ASSET_PATH_URL', 'http://http://paymentgateway.loc/cmb/');
 }
 
-
-
 define('NAME', 'Malkey Rent A Car');
-define('CC_LIST', ['_thamara.dasun1@gmail.com', '_piumal0713@gmail.com']);
+define('CC_LIST', ['milindum@gmail.com','accounts@malkey.lk','billing@malkey.lk','info@malkey.lk' ]);
+define('BCC_LIST', ['piumal0713@gmail.com','viraj.abayarathna@gmail.com',]);
 define('MAIL_DRIVER', 'smtp');
 define('MAIL_HOST', 'email-smtp.us-east-1.amazonaws.com');
 define('MAIL_PORT', 465);
@@ -71,4 +68,4 @@ define('MAIL_ENCRYPTION', 'ssl');
 define('MAIL_USERNAME', 'AKIA5K7Q37VYYJEFNMN2');
 define('MAIL_PASSWORD', 'BHwtncYWVjdoVtd5Y9Epu1/UBPV7fRi+zbblftJlqabg');
 define('MAIL_ADDRESS', 'rype3-dtaas-platform@rype3.com');
-define('MAIL_NAME', 'Test email');
+define('MAIL_NAME', 'Malkey Rent A Car');
