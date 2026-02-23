73f904d277778140f48b193b9733d26a2aed985a
badtux <viraj@ceynet.asia>
2026-02-23 13:17:29 +0530

    commeneted the STDERR

--- cron.php
+++ cron.php
@@ -529,7 +529,7 @@ foreach ($docs as $doc) {
             }
         }
 
-        fwrite(STDERR, "ERROR uuid={$uuid}, orderId={$orderId}: {$e->getMessage()}\n");
+        //fwrite(STDERR, "ERROR uuid={$uuid}, orderId={$orderId}: {$e->getMessage()}\n");
         echo "ERROR uuid={$uuid}, orderId={$orderId}: {$e->getMessage()}\n";
         continue;
     }
