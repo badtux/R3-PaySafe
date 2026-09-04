00a92a54d0a35a90af8b62ada29cfb89ca6cb923
yushapiumal <adypiumalhpbk0701@gmail.com>
2026-09-04 15:15:13 +0530

    change  cron config path

--- cron.php
+++ cron.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 $cliArgv = [];
 
 require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config/config.sample.php';
+require_once __DIR__ . '/config/config.php';
 
 use MongoDB\Client;
 use PHPMailer\PHPMailer\PHPMailer;
